7e2a6c3d009ef55cd37f13eefd04f89dfb540589
Dennis Laptop <mail@gmail.com>
2018-02-26 09:12:07 +0100

    Added Unity project with SteamVR, Oculus LipSync, and testmodel assets

---
+++ New Unity Project/Assets/Supercyan Character Pack Free Sample/Scripts/IKControlTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKControlTest : MonoBehaviour
+{
+
+	[SerializeField] Transform lookAtPos;
+	[SerializeField] Transform rightHandPos;
+	[SerializeField] Transform leftHandPos;
+	Animator anim;
+
+	// Use this for initialization
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void OnAnimatorIK()
+	{
+		if (anim)
+		{
+			anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+			anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+			anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);	
+			anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+			anim.SetLookAtWeight(1);
+
+			anim.SetLookAtPosition(lookAtPos.position);
+			anim.SetIKPosition(AvatarIKGoal.RightHand, rightHandPos.position);
+			anim.SetIKRotation(AvatarIKGoal.RightHand, rightHandPos.rotation);
+			anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandPos.position);
+			anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHandPos.rotation);
+		}
+	}
+}
