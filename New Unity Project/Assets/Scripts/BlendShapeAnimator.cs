cb1f1c5688ec6413c732ddd61f9457308bece8a5
Dennis Laptop <mail@gmail.com>
2018-03-08 15:43:01 +0100

    blend shape tests

---
+++ New Unity Project/Assets/Scripts/BlendShapeAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendShapeAnimator : MonoBehaviour
+{
+
+	[SerializeField] SkinnedMeshRenderer meshRenderer;
+	[SerializeField] float transitionSpeed = 100f;
+	int numOfBlendShapes;
+	int blendShapeIndex = 1;
+
+	void Awake()
+	{
+		numOfBlendShapes = meshRenderer.sharedMesh.blendShapeCount;
+		meshRenderer.SetBlendShapeWeight(0, 100);
+	}
+
+	void Update()
+	{
+		float currentWeight = meshRenderer.GetBlendShapeWeight(blendShapeIndex - 1) - transitionSpeed * Time.deltaTime;
+		if (currentWeight <= 0)
+		{
+			meshRenderer.SetBlendShapeWeight(blendShapeIndex - 1, 0);
+			meshRenderer.SetBlendShapeWeight(blendShapeIndex, 100);
+			blendShapeIndex++;
+			if (blendShapeIndex > numOfBlendShapes)
+				blendShapeIndex = 0;
+		}
+		else
+		{
+			meshRenderer.SetBlendShapeWeight(blendShapeIndex - 1, currentWeight);
+			meshRenderer.SetBlendShapeWeight(blendShapeIndex, 100 - currentWeight);
+		}
+	}
+}
