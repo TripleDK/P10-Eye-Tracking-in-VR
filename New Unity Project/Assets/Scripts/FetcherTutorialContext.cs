d0f97f86468d728ae5558aa7e5f214f87689f0ce
AvaLab B <>
2018-05-01 08:31:44 +0200

    Outcommented sound to make stuff work temporarily

--- New Unity Project/Assets/Scripts/FetcherTutorialContext.cs
+++ New Unity Project/Assets/Scripts/FetcherTutorialContext.cs
@@ -5,62 +5,62 @@ using TMPro;
 
 public class FetcherTutorialContext : MonoBehaviour
 {
-	[SerializeField] TextMeshPro textField;
-	[SerializeField] ObjectInteractions testObject;
-	[SerializeField] Transform testObjectStartPos;
-	[SerializeField] Teleporter teleporter;
-	[SerializeField] List<AudioClip> dialog = new List<AudioClip>();
-	ObjectInteractions spawnedTestObject;
+    [SerializeField] TextMeshPro textField;
+    [SerializeField] ObjectInteractions testObject;
+    [SerializeField] Transform testObjectStartPos;
+    [SerializeField] Teleporter teleporter;
+    [SerializeField] List<AudioClip> dialog = new List<AudioClip>();
+    ObjectInteractions spawnedTestObject;
 
-	void Awake()
-	{
-		textField.text = "";
-	}
+    void Awake()
+    {
+        textField.text = "";
+    }
 
-	public void StartTutorial()
-	{
-		textField.text = "Welcome \n-Fetcher- \n\nOverall goal: \nClose the black hole \n\nImportant tool: \nCommunicating";
-		AudioSource.PlayClipAtPoint(dialog[0], textField.transform.position);
-	}
+    public void StartTutorial()
+    {
+        textField.text = "Welcome \n-Fetcher- \n\nOverall goal: \nClose the black hole \n\nImportant tool: \nCommunicating";
+        //AudioSource.PlayClipAtPoint(dialog[0], textField.transform.position);
+    }
 
-	IEnumerator GrabObject()
-	{
-		yield return new WaitForSeconds(dialog[0].length);
-		textField.text = "Current goal: \nGrab red object \n\nImportant tool: \nOpposable thumb";
-		AudioSource.PlayClipAtPoint(dialog[1], textField.transform.position);
-		spawnedTestObject = Instantiate(testObject, testObjectStartPos.position, testObjectStartPos.rotation);
-		spawnedTestObject.transform.GetComponent<MeshRenderer>().material.color = Color.red;
-		spawnedTestObject.OnBallGrabbed.AddListener(MoveObjectToTeleporter);
-	}
+    IEnumerator GrabObject()
+    {
+        yield return new WaitForSeconds(dialog[0].length);
+        textField.text = "Current goal: \nGrab red object \n\nImportant tool: \nOpposable thumb";
+        //AudioSource.PlayClipAtPoint(dialog[1], textField.transform.position);
+        spawnedTestObject = Instantiate(testObject, testObjectStartPos.position, testObjectStartPos.rotation);
+        spawnedTestObject.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+        spawnedTestObject.OnBallGrabbed.AddListener(MoveObjectToTeleporter);
+    }
 
-	void MoveObjectToTeleporter(GameObject sender)
-	{
-		spawnedTestObject.OnBallGrabbed.RemoveListener(MoveObjectToTeleporter);
-		textField.text = "Current goal: \nPlace object in teleporter \n\nImportant tool: \nStrong throwing arm and good aim";
-		AudioSource.PlayClipAtPoint(dialog[2], textField.transform.position);
-		teleporter.OnAcceptItem.AddListener(StartTheTeleporter);
-	}
+    void MoveObjectToTeleporter(GameObject sender)
+    {
+        spawnedTestObject.OnBallGrabbed.RemoveListener(MoveObjectToTeleporter);
+        textField.text = "Current goal: \nPlace object in teleporter \n\nImportant tool: \nStrong throwing arm and good aim";
+        //AudioSource.PlayClipAtPoint(dialog[2], textField.transform.position);
+        teleporter.OnAcceptItem.AddListener(StartTheTeleporter);
+    }
 
-	void StartTheTeleporter()
-	{
-		teleporter.OnAcceptItem.RemoveListener(StartTheTeleporter);
-		textField.text = "Current goal: \nActivate teleporter \n\nImportant tool: \nCuriosity of big red buttons";
-		AudioSource.PlayClipAtPoint(dialog[3], textField.transform.position);
-		teleporter.OnTeleportItem.AddListener(CompleteTutorial);
-	}
+    void StartTheTeleporter()
+    {
+        teleporter.OnAcceptItem.RemoveListener(StartTheTeleporter);
+        textField.text = "Current goal: \nActivate teleporter \n\nImportant tool: \nCuriosity of big red buttons";
+        //AudioSource.PlayClipAtPoint(dialog[3], textField.transform.position);
+        teleporter.OnTeleportItem.AddListener(CompleteTutorial);
+    }
 
-	void CompleteTutorial()
-	{
-		teleporter.OnTeleportItem.RemoveListener(CompleteTutorial);
-		AudioSource.PlayClipAtPoint(dialog[4], textField.transform.position);
-		textField.text = "Test complete \n\nCurrent goal: \nWait for partner to be ready \n\nImportant tool: \nPatience";
-		TaskContext.singleton.FetcherTutDone();
-		StartCoroutine(TutorialFiveSecDelay());
-	}
+    void CompleteTutorial()
+    {
+        teleporter.OnTeleportItem.RemoveListener(CompleteTutorial);
+        //AudioSource.PlayClipAtPoint(dialog[4], textField.transform.position);
+        textField.text = "Test complete \n\nCurrent goal: \nWait for partner to be ready \n\nImportant tool: \nPatience";
+        TaskContext.singleton.FetcherTutDone();
+        StartCoroutine(TutorialFiveSecDelay());
+    }
 
-	IEnumerator TutorialFiveSecDelay()
-	{
-		yield return new WaitForSeconds(5);
-		Destroy(textField.gameObject);
-	}
+    IEnumerator TutorialFiveSecDelay()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(textField.gameObject);
+    }
 }
--- New Unity Project/Assets/Scripts/FixerTutorialContext.cs
+++ New Unity Project/Assets/Scripts/FixerTutorialContext.cs
@@ -5,70 +5,70 @@ using TMPro;
 
 public class FixerTutorialContext : MonoBehaviour
 {
-	[SerializeField] TextMeshPro textField;
-	[SerializeField] GazeDirection gaze;
-	[SerializeField] ObjectInteractions testObject;
-	[SerializeField] Transform testObjectStartPos;
-	[SerializeField] Transform testObjectPreviewPos;
-	[SerializeField] TextMeshPro previewText;
-	[SerializeField] BlackHole blackHole;
-	[SerializeField] List<AudioClip> dialogue = new List<AudioClip>();
-	ObjectInteractions spawnedTestObject;
-	ObjectInteractions spawnedTestObjectPreview;
+    [SerializeField] TextMeshPro textField;
+    [SerializeField] GazeDirection gaze;
+    [SerializeField] ObjectInteractions testObject;
+    [SerializeField] Transform testObjectStartPos;
+    [SerializeField] Transform testObjectPreviewPos;
+    [SerializeField] TextMeshPro previewText;
+    [SerializeField] BlackHole blackHole;
+    [SerializeField] List<AudioClip> dialogue = new List<AudioClip>();
+    ObjectInteractions spawnedTestObject;
+    ObjectInteractions spawnedTestObjectPreview;
 
-	void Awake()
-	{
-		textField.text = "";
-	}
+    void Awake()
+    {
+        textField.text = "";
+    }
 
-	public void StartTutorial()
-	{
-		textField.text = "Welcome: -Fixer- \n\nOverall goal: \nClose the black hole \n\nImportant tool: \nCommuncating";
-		AudioSource.PlayClipAtPoint(dialogue[0], textField.transform.position);
-		StartCoroutine(LookAtMonitor());
-	}
+    public void StartTutorial()
+    {
+        textField.text = "Welcome: -Fixer- \n\nOverall goal: \nClose the black hole \n\nImportant tool: \nCommuncating";
+        //AudioSource.PlayClipAtPoint(dialogue[0], textField.transform.position);
+        StartCoroutine(LookAtMonitor());
+    }
 
-	IEnumerator LookAtMonitor()
-	{
-		yield return new WaitForSeconds(dialogue[0].length);
-		AudioSource.PlayClipAtPoint(dialogue[1], textField.transform.position);
-		textField.text = "Current goal: \nLocate monitor \n\nImportant tool: \nSpatial awareness";
-		gaze.OnTVRealized.AddListener(GrabAnObject);
-	}
+    IEnumerator LookAtMonitor()
+    {
+        yield return new WaitForSeconds(dialogue[0].length);
+        //AudioSource.PlayClipAtPoint(dialogue[1], textField.transform.position);
+        textField.text = "Current goal: \nLocate monitor \n\nImportant tool: \nSpatial awareness";
+        gaze.OnTVRealized.AddListener(GrabAnObject);
+    }
 
-	public void GrabAnObject()
-	{
-		gaze.OnTVRealized.RemoveListener(GrabAnObject);
-		spawnedTestObjectPreview = Instantiate(testObject, testObjectPreviewPos.position, testObjectPreviewPos.rotation);
-		previewText.text = "Test object";
-		AudioSource.PlayClipAtPoint(dialogue[2], textField.transform.position);
-		textField.text = "Current goal: \nLocate and pick up test object \n\nImportant tool: \nGaze movement";
-		spawnedTestObject = Instantiate(testObject, testObjectStartPos.position, testObjectStartPos.rotation);
-		spawnedTestObject.OnBallGrabbed.AddListener(ThrowInBlackHole);
-	}
+    public void GrabAnObject()
+    {
+        gaze.OnTVRealized.RemoveListener(GrabAnObject);
+        spawnedTestObjectPreview = Instantiate(testObject, testObjectPreviewPos.position, testObjectPreviewPos.rotation);
+        previewText.text = "Test object";
+        //AudioSource.PlayClipAtPoint(dialogue[2], textField.transform.position);
+        textField.text = "Current goal: \nLocate and pick up test object \n\nImportant tool: \nGaze movement";
+        spawnedTestObject = Instantiate(testObject, testObjectStartPos.position, testObjectStartPos.rotation);
+        spawnedTestObject.OnBallGrabbed.AddListener(ThrowInBlackHole);
+    }
 
-	void ThrowInBlackHole(GameObject sender)
-	{
-		spawnedTestObject.OnBallGrabbed.RemoveListener(ThrowInBlackHole);
-		AudioSource.PlayClipAtPoint(dialogue[3], textField.transform.position);
-		textField.text = "Current goal: \nThrow test object into black hole \n\nImportant tool: \nArm strength";
-		blackHole.OnEatObject.AddListener(FinishTutorial);
-	}
+    void ThrowInBlackHole(GameObject sender)
+    {
+        spawnedTestObject.OnBallGrabbed.RemoveListener(ThrowInBlackHole);
+        //AudioSource.PlayClipAtPoint(dialogue[3], textField.transform.position);
+        textField.text = "Current goal: \nThrow test object into black hole \n\nImportant tool: \nArm strength";
+        blackHole.OnEatObject.AddListener(FinishTutorial);
+    }
 
-	void FinishTutorial()
-	{
-		blackHole.OnEatObject.RemoveListener(FinishTutorial);
-		previewText.text = "";
-		AudioSource.PlayClipAtPoint(dialogue[4], textField.transform.position);
-		Destroy(spawnedTestObjectPreview);
-		textField.text = "Test complete \n\nCurrent goal: \nWait for partner to be ready \n\nImportant tool: \nPatience";
-		TaskContext.singleton.FixerTutDone();
-		StartCoroutine(TutorialFiveSecDelay());
-	}
+    void FinishTutorial()
+    {
+        blackHole.OnEatObject.RemoveListener(FinishTutorial);
+        previewText.text = "";
+        //AudioSource.PlayClipAtPoint(dialogue[4], textField.transform.position);
+        Destroy(spawnedTestObjectPreview);
+        textField.text = "Test complete \n\nCurrent goal: \nWait for partner to be ready \n\nImportant tool: \nPatience";
+        TaskContext.singleton.FixerTutDone();
+        StartCoroutine(TutorialFiveSecDelay());
+    }
 
-	IEnumerator TutorialFiveSecDelay()
-	{
-		yield return new WaitForSeconds(5);
-		Destroy(textField.gameObject);
-	}
+    IEnumerator TutorialFiveSecDelay()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(textField.gameObject);
+    }
 }
