7688991c0b9df8793c79fadd72938cea2ce66b83
AvaLab B <>
2018-05-04 03:01:42 +0200

    implemented sound for likert button

--- New Unity Project/Assets/Scripts/LikertButton.cs
+++ New Unity Project/Assets/Scripts/LikertButton.cs
@@ -11,9 +11,11 @@ public class LikertButton : VRButton
 {
     public int likertValue = 0;
     public UnityIntEvent OnSelected = new UnityIntEvent();
+    [SerializeField] AudioClip clickSound;
 
     public override void Action(Controller side)
     {
         OnSelected.Invoke(likertValue);
+        AudioSource.PlayClipAtPoint(clickSound, transform.position);
     }
 }
